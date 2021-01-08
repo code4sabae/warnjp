387da174373d27df442c5245ba1ff03bde6e352e
Taisuke Fukuno <fukuno@jig.jp>
2021-01-09 08:39:50 +0900

    update design

--- run.js
+++ run.js
@@ -4,17 +4,11 @@ import { downloadPref } from "./downloadPref.js";
 import { parsePrefAll } from "./parsePref.js";
 import { getDateByTenMinutes } from "./getDate.js";
 
-const update = async () => {
-    const nowdt = getDateByTenMinutes();
-        try {
-            await Deno.mkdir("temp/csv", { recursive: true });
-            await downloadIndex();
-            await parseIndex();
-            await downloadPref();
-            await parsePrefAll(nowdt);
-            console.log("updated!", nowdt);
-        } catch (e) {
-            console.log(e);
-        }
-};
-await update();
+await Deno.mkdir("temp/csv", { recursive: true });
+
+const nowdt = getDateByTenMinutes();
+
+await downloadIndex();
+await parseIndex();
+await downloadPref();
+await parsePrefAll(nowdt);
